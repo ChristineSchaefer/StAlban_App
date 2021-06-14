f44bdc5ca620f47b15e5ec32e1f4f638204f90ef
ChristineSchaefer <82021777+ChristineSchaefer@users.noreply.github.com>
2021-06-14 17:09:18 +0200

    working progress gallery

--- app/src/main/java/com/example/stalban_app/sta_gallery/sta_gallery_activity.java
+++ app/src/main/java/com/example/stalban_app/sta_gallery/sta_gallery_activity.java
@@ -1,17 +1,22 @@
 package com.example.stalban_app.sta_gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.stalban_app.R;
 
 public class sta_gallery_activity extends AppCompatActivity {
-    /*protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sta_galery_menu);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.header_menu);
-        setSupportActionBar(myToolbar);
-    }*/
+        setContentView(R.layout.sta_gallery_menu);
+
+        Button galleryWar = (Button) findViewById(R.id.gallery_war);
+        galleryWar.setOnClickListener(v -> startActivity(new Intent(sta_gallery_activity.this, sta_gallery_activity_war.class)));
+
+        Button galleryArt = (Button) findViewById(R.id.gallery_art);
+        galleryArt.setOnClickListener(v -> startActivity(new Intent(sta_gallery_activity.this, sta_gallery_activity_art.class)));
+    }
 }
---
+++ app/src/main/java/com/example/stalban_app/sta_gallery/sta_gallery_activity_war.java
@@ -0,0 +1,14 @@
+package com.example.stalban_app.sta_gallery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.stalban_app.R;
+
+public class sta_gallery_activity_war extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sta_gallery_war);
+    }
+}
