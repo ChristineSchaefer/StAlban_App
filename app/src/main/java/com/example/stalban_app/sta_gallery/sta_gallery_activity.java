05e3c92316eff8f30d669ae046eec1275eeab75d
ChristineSchaefer <82021777+ChristineSchaefer@users.noreply.github.com>
2021-06-14 16:23:09 +0200

    working progress gallery

---
+++ app/src/main/java/com/example/stalban_app/sta_gallery/sta_gallery_activity.java
@@ -0,0 +1,17 @@
+package com.example.stalban_app.sta_gallery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.stalban_app.R;
+
+public class sta_gallery_activity extends AppCompatActivity {
+    /*protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sta_galery_menu);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.header_menu);
+        setSupportActionBar(myToolbar);
+    }*/
+}
