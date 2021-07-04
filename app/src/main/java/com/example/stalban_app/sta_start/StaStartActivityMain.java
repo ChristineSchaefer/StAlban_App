013e47ff7e13cd1e8f261c44e451a18b038742d8
Benedikte <85172592+Bloebber@users.noreply.github.com>
2021-07-04 15:37:49 +0200

    Vorläufig Activities

--- app/src/main/java/com/example/stalban_app/sta_start/StaStartActivityMain.java
+++ app/src/main/java/com/example/stalban_app/sta_start/StaStartActivityMain.java
@@ -1,4 +1,28 @@
 package com.example.stalban_app.sta_start;
 
-public class StaStartActivityMain {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.stalban_app.R;
+import com.example.stalban_app.sta_gallery.StaGalleryActivityMain;
+import com.example.stalban_app.sta_gallery.StaGalleryActivityView;
+
+public class StaStartActivityMain extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sta_start);
+
+        Button app_start = findViewById(R.id.sta_start_constraint);
+        app_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(StaStartActivityMain.this, StaStartActivityMain.class);
+                startActivity(i);
+            }
+        });
+    }
 }
