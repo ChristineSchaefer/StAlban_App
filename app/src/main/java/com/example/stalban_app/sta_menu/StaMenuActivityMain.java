3373ab2f2c8f4c31a51cf86e55d83b92dd8639bd
Benedikte <85172592+Bloebber@users.noreply.github.com>
2021-07-13 13:52:17 +0200

    Warnings und bisschen Deko

--- app/src/main/java/com/example/stalban_app/sta_menu/StaMenuActivityMain.java
+++ app/src/main/java/com/example/stalban_app/sta_menu/StaMenuActivityMain.java
@@ -2,15 +2,12 @@ package com.example.stalban_app.sta_menu;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.stalban_app.R;
 import com.example.stalban_app.sta_gallery.StaGalleryActivityMain;
-import com.example.stalban_app.sta_gallery.StaGalleryActivityView;
-import com.example.stalban_app.sta_start.StaStartActivityMain;
 import com.example.stalban_app.sta_timeline.StaTimelineActivityMain;
 import com.example.stalban_app.sta_tour.StaTourActivityMain;
 
@@ -22,32 +19,25 @@ public class StaMenuActivityMain extends AppCompatActivity {
 
         /*
         Button Tour
-        Hier noch Zielklasse ändern!
          */
         Button tour = findViewById(R.id.menu_button_1);
-        tour.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(StaMenuActivityMain.this, StaTourActivityMain.class);
-                startActivity(i);
-            }
+        tour.setOnClickListener(v -> {
+            Intent i = new Intent(StaMenuActivityMain.this, StaTourActivityMain.class);
+            startActivity(i);
         });
 
         // Button Bildergallerie
         Button galleryArt = findViewById(R.id.menu_button_2);
-        galleryArt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(StaMenuActivityMain.this, StaGalleryActivityMain.class);
-                startActivity(i);
-            }
+        galleryArt.setOnClickListener(v -> {
+            Intent i = new Intent(StaMenuActivityMain.this, StaGalleryActivityMain.class);
+            startActivity(i);
         });
 
         // Button Timeline
         Button timeLine = findViewById(R.id.menu_button_3);
-        timeLine.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(StaMenuActivityMain.this, StaTimelineActivityMain.class);
-                startActivity(i);
-            }
+        timeLine.setOnClickListener(v -> {
+            Intent i = new Intent(StaMenuActivityMain.this, StaTimelineActivityMain.class);
+            startActivity(i);
         });
 
         /*
@@ -55,11 +45,9 @@ public class StaMenuActivityMain extends AppCompatActivity {
         Hier noch Zielklasse ändern
          */
         Button impressum = findViewById(R.id.menu_button_4);
-        impressum.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(StaMenuActivityMain.this, StaMenuActivityMain.class);
-                startActivity(i);
-            }
+        impressum.setOnClickListener(v -> {
+            Intent i = new Intent(StaMenuActivityMain.this, StaMenuActivityMain.class);
+            startActivity(i);
         });
 
     }
