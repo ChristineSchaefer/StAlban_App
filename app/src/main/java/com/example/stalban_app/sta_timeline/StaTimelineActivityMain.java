6323f60552ba023c715fca54370d8fdcebd03c38
Anna-Maria <Anna-Maria.Evers@gmx.de>
2021-07-14 16:24:15 +0200

    Timeline List View

--- app/src/main/java/com/example/stalban_app/sta_timeline/StaTimelineActivityMain.java
+++ app/src/main/java/com/example/stalban_app/sta_timeline/StaTimelineActivityMain.java
@@ -1,15 +1,32 @@
 package com.example.stalban_app.sta_timeline;
-
-
+import android.app.ListActivity;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.example.stalban_app.R;
 
-public class StaTimelineActivityMain extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState) {
+public class StaTimelineActivityMain extends ListActivity {
+    String[] eintrag = {
+            "Eintrag 1",
+            "Eintrag 2",
+            "Eintrag 3",
+            "Eintrag 4",
+            "Eintrag 5",
+            "Eintrag 6",
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sta_timeline);
+        //Listview adapter
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,eintrag));
+    }
+    public void onListItemClick(ListView parent, View v,
+                                int position, long id) {
+        Toast.makeText(this, "Ihre Auswahl : " + eintrag[position],
+                Toast.LENGTH_LONG).show();
     }
 }
